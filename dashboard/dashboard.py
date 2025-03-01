e38037c17b992ff55a6a8f0b4bcfd6a8b420a9d6
Rama <ramaadjiprsty@gmail.com>
2025-03-01 17:02:42 +0700

    change folder structure and dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,6 +5,8 @@ import streamlit as st
 import geopandas as gpd
 from shapely.geometry import Point
 
+colors = ["#e80909", "#cf6363", "#cf6363", "#cf6363", "#cf6363", "#cf6363", "#cf6363", "#cf6363", "#cf6363", "#cf6363"]
+
 def create_top_customer_cities(df):
     bycity_df = df.groupby("customer_city").customer_id.nunique().reset_index()
     bycity_df.rename(columns={
@@ -37,9 +39,9 @@ def create_most_sold_product(df):
 
     return most_sold_product
 
-all_df = pd.read_csv("result_df/all_df.csv")
-product_df = pd.read_csv("result_df/translated_most_sold_product_category.csv")
-customer_in_geolocation_df = pd.read_csv("result_df/customer_in_geolocation.csv")
+all_df = pd.read_csv("all_df.csv")
+product_df = pd.read_csv("translated_most_sold_product_category.csv")
+customer_in_geolocation_df = pd.read_csv("customer_in_geolocation.csv")
 
 
 top_customer_cities_df = create_top_customer_cities(all_df)
@@ -49,29 +51,48 @@ most_sold_product_df = create_most_sold_product(product_df)
 
 st.header("e-Commerce Dashboard")
 
-# Chart for customer distribution based on cities
-st.subheader("Top 10 Cities with the Highest Number of Customers")
+st.subheader("Top 10 States and Cities with the Highest Number of Customers ")
 
-fig, ax = plt.subplots(figsize=(16, 8))
-
-sns.barplot(
-    y="customer_city",
-    x="customer_count",
-    data=top_customer_cities_df.sort_values(by="customer_count", ascending=False).head(10),
-    palette="rocket",
-    ax=ax
+category = st.selectbox(
+    "Select Categorical Column",
+    ['customer_city', 'customer_state',]
 )
 
-ax.set_title("Top 10 Cities with the Highest Number of Customers", fontsize=15)
-ax.set_ylabel("City", fontsize=20)
-ax.set_xlabel("Total Customers", fontsize=20)
-ax.grid(axis="x", linestyle="--", color="black", alpha=0.7)
-ax.tick_params(axis='x', labelsize=12)
-st.pyplot(fig)
+chart_type = st.selectbox("Select Chart Type", ['Bar Chart', 'Pie Chart'])
+
+st.subheader(f"{category} distribution")
+
+fig, ax = plt.subplots(figsize=(10, 6))
+
+if chart_type == 'Bar Chart':
+    sns.countplot(
+        data=product_df,
+        x=category,
+        palette=colors,
+        order=product_df[category].value_counts().head(10).index,
+        ax=ax
+    )
+
+    ax.set_title(f"{category} Count Distribution")
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+    plt.xticks(rotation=45)
+    st.pyplot(fig)
+
+elif chart_type == 'Pie Chart':
+    pie_data = product_df[category].value_counts().head(10)
+    ax.pie(
+        pie_data,
+        labels=pie_data.index,
+        autopct='%1.1f%%',
+        colors=sns.color_palette('viridis', len(pie_data))
+    )
+    ax.set_title(f"{category} Percentage Distribution")
+    st.pyplot(fig)
 
 # Chart for delivered order percentage
 st.subheader("Order Status Distribution Percentage")
-fig, ax = plt.subplots(figsize=(16, 10))
+fig, ax = plt.subplots(figsize=(10, 6))
 plt.pie(
     by_order_status_df["order_id"],
     labels=by_order_status_df["order_status"],
@@ -94,7 +115,7 @@ st.pyplot(fig)
 
 
 st.subheader("Order Status Distribution Percentage")
-fig, ax = plt.subplots(figsize=(16, 10))
+fig, ax = plt.subplots(figsize=(10, 6))
 wedges, texts, autotexts = plt.pie(
     by_payment_type_df,
     labels=by_payment_type_df.index,
@@ -123,13 +144,15 @@ st.pyplot(fig)
 st.subheader("Top 10 Best-Selling Products and Highest Revenue")
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 25))
 
-# Plot for Top 10 Best-Selling Products
+
 sns.barplot(
     ax=ax[0],
     x="order_id",
     y="product_category_name_english",
     data=most_sold_product_df.head(10),
-    palette="viridis"
+    # palette="viridis"
+    # hue="product_category_name_english",
+    palette=colors
 )
 ax[0].set_ylabel(None)
 ax[0].set_xlabel("Total Sold", fontsize=30)
@@ -144,7 +167,9 @@ sns.barplot(
     x="price",
     y="product_category_name_english",
     data=most_sold_product_df.sort_values(by="price", ascending=False).head(10),
-    palette="mako"
+    # palette="mako"
+    # hue="product_category_name_english",
+    palette=colors
 )
 ax[1].set_ylabel(None)
 ax[1].set_xlabel("Total Revenue in Million ($)", fontsize=30)
@@ -162,22 +187,17 @@ st.subheader("Customer Distribution based on State")
 
 customer_in_geolocation_df["geometry"] = customer_in_geolocation_df.apply(lambda x: Point(x["geolocation_lng"], x["geolocation_lat"]), axis=1)
 
-# Convert to GeoDataFrame
 geo_df = gpd.GeoDataFrame(customer_in_geolocation_df, geometry="geometry")
 
-# Calculate percentage of customers per state
 state_counts = geo_df['customer_state'].value_counts()
 total_customers = len(geo_df)
 state_percentages = (state_counts / total_customers) * 100
 
-# Plot data with color based on customer_state
 fig, ax = plt.subplots(figsize=(15, 8))
 geo_df.plot(marker='o', column='customer_state', cmap='tab20', markersize=2, alpha=0.6, legend=True, ax=ax)
 
-# Move legend position
 ax.get_legend().set_bbox_to_anchor((1, 1))
 
-# Add percentage labels to the legend
 legend = ax.get_legend()
 if legend:
     for text in legend.get_texts():
